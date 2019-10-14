a3e51deca1b2f1ff24765c19b18d62bf0864dcf1
EhanAhmed <55330822+EhanAhmed@users.noreply.github.com>
2019-10-14 21:11:24 +0500

    Add files via upload

---
+++ pengram.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, Alphastr As String
+        Dim nextchar As Char
+        Dim count As Integer
+        Dim ispangram As Boolean
+
+        str1 = ""
+        Alphastr = "abcdefghijklmnopqrstuvwxyz"
+        nextchar = ""
+        count = 0
+        ispangram = True
+
+        Console.WriteLine("enterstring")
+        str1 = Console.ReadLine
+        str1 = LCase(str1)
+
+        For count = 1 To Len(Alphastr)
+            nextchar = Mid(Alphastr, count, 1)
+            If InStr(str1, nextchar) = 0 Then
+                ispangram = False
+                Exit For
+            End If
+
+        Next
+
+        If ispangram = True Then
+            Console.WriteLine("statement is pengram")
+        Else
+            Console.WriteLine("statement is not pengram")
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
