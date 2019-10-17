b78850219ed3c6c02967886e3fcce4134625ca95
EhanAhmed <55330822+EhanAhmed@users.noreply.github.com>
2019-10-17 21:15:52 +0500

    Add files via upload

---
+++ test 1a .vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim binarynumber As String
+        Dim validbinarystring As Boolean
+        Dim count As Integer
+        Dim nextchar As String
+
+        binarynumber = ""
+        validbinarystring = True
+        count = 0
+        nextchar = ""
+
+        Console.WriteLine("enter binary number")
+        binarynumber = Console.ReadLine
+
+
+        For count = 1 To Len(binarynumber)
+            If Len(binarynumber) <> 8 Then
+                validbinarystring = False
+            End If
+        Next
+
+        For count = 1 To Len(binarynumber)
+            nextchar = Mid(binarynumber, count, 1)
+            If nextchar <> 0 Or nextchar <> 1 Then
+                validbinarystring = False
+
+            End If
+
+        Next
+
+        If validbinarystring = True Then
+            Console.Write("valid string")
+        Else
+            Console.Write("Invalid string")
+        End If
+    End Sub
+
+End Module
